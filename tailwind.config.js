da3e1930edf2b227f312991c3c9413a28f604d72
2hats-varsha <varshac@2hatslogic.com>
2025-04-07 17:00:05 +0530

    second section

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,43 +2,35 @@
 const plugin = require('tailwindcss/plugin');
 
 module.exports = {
-  content: ["./src/**/*.{html,js,vue,jsx,ts,tsx}"],
+  content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
       colors: {
-        'primary': '#4D2C5E',
-        'secondary': '#FF7426',
-        'white': '#fff',
-        'black': '#000',
-        'gray': '#707070',
-        'blue': '#0F335E',
+        primary: '#4D2C5E',
+        secondary: '#FF7426',
+        white: '#fff',
+        black: '#000',
+        gray: '#707070',
+        blue: '#0F335E',
       },
-      backgroundColor: theme => ({
-        'primary': theme('colors.primary'),
-        'secondary': theme('colors.secondary'),
-        'white': theme('colors.white'),
-        'black': theme('colors.black'),
-        'gray': theme('colors.gray'),
-        'blue': theme('colors.blue'),
-      }),
       fontFamily: {
         roboto: ['Roboto', 'sans-serif'],
       },
       fontSize: {
-        'h1': '4.3125rem',
-        'h2': '50px',
-        'h3': '40px',
-        'h4': '30px',
-        'h5': '28px',
-        'h6': '24px',
-        'p': '16px',
-        'xxs': '10px',
-        'xs': '12px',
-        'sm': '14px',
-        'base': '16px',
-        'md': '18px',
-        'lg': '20px',
-        'xl': '22px',
+        h1: '4.3125rem',
+        h2: '50px',
+        h3: '40px',
+        h4: '30px',
+        h5: '28px',
+        h6: '24px',
+        p: '16px',
+        xxs: '10px',
+        xs: '12px',
+        sm: '14px',
+        base: '16px',
+        md: '18px',
+        lg: '20px',
+        xl: '22px',
         '2xl': '24px',
         '3xl': '26px',
         '4xl': '28px',
@@ -60,8 +52,8 @@ module.exports = {
         '20xl': '60px',
       },
       fontWeight: {
-        regular: '500',
-        medium: '600',
+        regular: '600',
+        medium: '500',
         semibold: '700',
         bold: '800',
       },
@@ -87,53 +79,54 @@ module.exports = {
     },
   },
   plugins: [
-    plugin(function ({ addBase, theme }) {
+    plugin(function ({ addBase }) {
       addBase({
         '@font-face': [
           {
             fontFamily: 'Roboto',
             fontStyle: 'normal',
-            fontWeight: '500',
+            fontWeight: '600',
             fontDisplay: 'swap',
-            src: "url('../src/assets/fonts/AnyConv.com__Roboto-Regular.woff2') format('woff2')",
+            src: "url('../fonts/Roboto-Regular.woff2') format('woff2')",
           },
           {
             fontFamily: 'Roboto',
             fontStyle: 'normal',
-            fontWeight: '600',
+            fontWeight: '500',
             fontDisplay: 'swap',
-            src: "url('../src/assets/fonts/AnyConv.com__Roboto-Medium.woff2') format('woff2')",
+            src: "url('../fonts/Roboto-Medium.woff2') format('woff2')",
           },
           {
             fontFamily: 'Roboto',
             fontStyle: 'normal',
             fontWeight: '700',
             fontDisplay: 'swap',
-            src: "url('../src/assets/fonts/AnyConv.com__Roboto-SemiBold.woff2') format('woff2')",
+            src: "url('../fonts/Roboto-SemiBold.woff2') format('woff2')",
           },
           {
             fontFamily: 'Roboto',
             fontStyle: 'normal',
             fontWeight: '800',
             fontDisplay: 'swap',
-            src: "url('../src/assets/fonts/Roboto-Bold.woff2') format('woff2')",
+            src: "url('../fonts/Roboto-Bold.woff2') format('woff2')",
           },
+         
           {
             fontFamily: 'icomoon',
             fontStyle: 'normal',
             fontWeight: 'normal',
             fontDisplay: 'block',
             src: `
-              url('../src/assets/icons/icomoon.eot?psrmvm');
-              url('../src/assets/icons/icomoon.eot?psrmvm#iefix') format('embedded-opentype'),
-              url('../src/assets/icons/icomoon.ttf?psrmvm') format('truetype'),
-              url('../src/assets/icons/icomoon.woff?psrmvm') format('woff'),
-              url('../src/assets/icons/icomoon.svg?psrmvm#icomoon') format('svg')
+              url('../icons/icomoon.eot?psrmvm');
+              url('../icons/icomoon.eot?psrmvm#iefix') format('embedded-opentype'),
+              url('../icons/icomoon.ttf?psrmvm') format('truetype'),
+              url('../icons/icomoon.woff?psrmvm') format('woff'),
+              url('../icons/icomoon.svg?psrmvm#icomoon') format('svg')
             `,
           },
         ],
         body: {
-          fontFamily: theme('fontFamily.roboto'),
+          fontFamily: 'Roboto, sans-serif',
         },
       });
     }),
